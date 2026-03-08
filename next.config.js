235a16a58abe4a59c51f0434063b41a548626fcc
Abdenourhe <abdenour.hellas@uqat.ca>
2026-03-08 12:48:55 -0400

    Simplify next.config.js by removing intl plugin
    Removed internationalization plugin and updated config.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
-const createNextIntlPlugin = require("next-intl/plugin");
-
-const withNextIntl = createNextIntlPlugin("./src/i18n.ts");
-
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "utfs.io",
-      },
-    ],
   },
+  basePath: "/CCIM-QURAN",
+  assetPrefix: "/CCIM-QURAN/",
 };
 
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig;
