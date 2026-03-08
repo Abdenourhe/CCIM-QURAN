c1ac930ff4c45246d4485e33afc4eab346c4e5ac
Abdenourhe <abdenour.hellas@uqat.ca>
2026-03-08 12:44:20 -0400

    Add output option and unoptimized images to config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const createNextIntlPlugin = require("next-intl/plugin");
 const withNextIntl = createNextIntlPlugin("./src/i18n.ts");
 
 const nextConfig = {
+  output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
